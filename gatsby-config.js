c8dabe0af5670a54cf29ca8c76468c2c5a971467
Tim Perry <pimterry@gmail.com>
2019-01-22 15:45:43 +0100

    Remove extra padding from responsive images in RSS

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,7 +52,12 @@ module.exports = {
                 // Hack to make image URLs absolute
                 const html = edge.node.html
                   .replace(/ src="\/static/g, ' src="https://httptoolkit.tech/static')
-                  .replace(/ href="\/static/g, ' href="https://httptoolkit.tech/static');
+                  .replace(/ href="\/static/g, ' href="https://httptoolkit.tech/static')
+                  + `<style>
+                    .gatsby-resp-image-background-image {
+                      padding-bottom: 0 !important;
+                    }
+                  </style>`;
 
                 return Object.assign({}, edge.node.frontmatter, {
                   description: html,
