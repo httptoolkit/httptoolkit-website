0145e1b9851a9bb5b5f11f6dba023ca3ad2a2a0c
Tim Perry <pimterry@gmail.com>
2019-04-27 13:39:38 +0200

    Ensure 404 links to the View page, not the old Toolkit page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -53,7 +53,7 @@ export default () => (<ThemeProvider theme={theme}>
             <ErrorDetails>
                 Oh no! Sorry, this page doesn't exist.
             </ErrorDetails>
-            <HomeLink to="/toolkit">
+            <HomeLink to="/view">
             Take me to the home page
             </HomeLink>
         </ErrorContainer>
