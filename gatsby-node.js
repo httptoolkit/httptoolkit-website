9f3919b36c24ca48ac817a7658aab9477fe59b5f
Tim Perry <pimterry@gmail.com>
2019-09-24 18:47:21 +0200

    Release the new desktop version (terminal fixes)

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs-extra');
 const { createFilePath } = require('gatsby-source-filesystem');
 
-const LATEST_VERSION = '0.1.12';
+const LATEST_VERSION = '0.1.13';
 
 // Include Monaco:
 exports.onPostBootstrap = function (pages) {
