f2dd953ecbcb4ba7c26d06a01d8a1dd2b0305573
Tim Perry <pimterry@gmail.com>
2019-11-04 23:39:24 +0100

    Fix mobile layout of JS target pages

--- src/components/per-target/js-supported-tools.jsx
+++ src/components/per-target/js-supported-tools.jsx
@@ -3,15 +3,16 @@ import * as React from 'react';
 import { SupportedTools, SupportedTool } from "./supported-tools";
 
 export const JSSupportedTools = () => <SupportedTools>
-    <SupportedTool>Built-in 'http' & 'https' modules</SupportedTool>
-    <SupportedTool>npm</SupportedTool>
+    <SupportedTool>Built-in 'http' module</SupportedTool>
+    <SupportedTool>Built-in 'https' module</SupportedTool>
     <SupportedTool>Request</SupportedTool>
     <SupportedTool>Axios</SupportedTool>
     <SupportedTool>Superagent</SupportedTool>
-    <SupportedTool>Fetch and Node-Fetch</SupportedTool>
+    <SupportedTool>Fetch & Node-Fetch</SupportedTool>
     <SupportedTool>Reqwest</SupportedTool>
     <SupportedTool>Got</SupportedTool>
     <SupportedTool>Needle</SupportedTool>
     <SupportedTool>Bent</SupportedTool>
     <SupportedTool>Unirest</SupportedTool>
+    <SupportedTool>npm</SupportedTool>
 </SupportedTools>
