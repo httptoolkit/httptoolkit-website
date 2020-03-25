e6dd1497050a2d21c5f465f5fe4c78fc4d4e51ad
Tim Perry <pimterry@gmail.com>
2020-03-25 14:13:33 +0100

    Load prices from accounts.httptoolkit.tech
    This speeds up price loading, reduces possible failures from the
    Paddle API, and lets us move to Paddle API v2 internally, which
    makes price formatting & localization much easier.

--- src/accounts/subscriptions.js
+++ src/accounts/subscriptions.js
@@ -22,12 +22,51 @@ const waitForPaddle = new Promise((resolve) => {
     checkForPaddle();
 });
 
-function getPlanPrices(planId) {
-    return waitForPaddle.then((paddle) =>
-        new Promise((resolve) => {
-            paddle.Product.Prices(planId, 1, resolve);
-        })
+function formatPrice(currency, price) {
+    return Number(price).toLocaleString(undefined, {
+        style:"currency",
+        currency: currency,
+        minimumFractionDigits: _.round(price) === price ? 0 : 2,
+        maximumFractionDigits: 2
+    })
+}
+
+async function loadPlanPrices() {
+    const response = await fetch(
+        `https://accounts.httptoolkit.tech/.netlify/functions/get-prices?product_ids=${
+            Object.values(SubscriptionPlans).map(plan => plan.id).join(',')
+        }`
     );
+
+    if (!response.ok) {
+        console.log(response);
+        throw new Error(`Failed to look up prices, got ${response.status}: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+
+    if (!data.success) {
+        console.log(data);
+        throw new Error("Price lookup request was unsuccessful");
+    }
+
+    const productPrices = data.response.products;
+
+    productPrices.forEach((productPrice) => {
+        const plan = _.find(SubscriptionPlans, { id: productPrice.product_id });
+        if (!plan) return;
+
+        const currency = productPrice.currency;
+        const totalPrice = productPrice.price.net;
+        const monthlyPrice = productPrice.subscription.interval === 'year'
+            ? totalPrice / 12
+            : totalPrice;
+
+        plan.prices = {
+            total: formatPrice(currency, totalPrice),
+            monthly: formatPrice(currency, monthlyPrice)
+        };
+    });
 }
 
 export const SubscriptionPlans = observable({
@@ -37,23 +76,12 @@ export const SubscriptionPlans = observable({
     'team-annual': { id: 550788 },
 });
 
-_.map(SubscriptionPlans, (PlanDetails) => {
-    getPlanPrices(PlanDetails.id).then((planPricing) => {
-        const planPrice = planPricing.price.net.replace(/[.,]00$/g, '');
-        const monthlyPrice = planPricing.recurring.subscription.type === 'year'
-            ? planPrice.replace(
-                /[\d.,]+$/g,
-                (d) => _.round((parseFloat(
-                    // Replace all punctuation except the last. This ensures we can parse
-                    // numbers in the 1,000s without assuming either . or , separators.
-                    d.replace(/[,.](?=\d+[,.])/g, '')
-                ) / 12), 2).toString()
-            )
-            : planPrice;
-
-        PlanDetails.prices = { total: planPrice, monthly: monthlyPrice };
-    });
-});
+if (!isSSR) {
+    // In the UI, we async load the plan prices onto the plan object
+    // above, using the data from the Paddle API.
+    // Prices are localized, so we can't do this during SSR.
+    loadPlanPrices();
+}
 
 export const getSubscriptionPlanCode = (id) =>
     _.findKey(SubscriptionPlans, { id: id });
