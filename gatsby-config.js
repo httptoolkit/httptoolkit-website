5e917bac2a083b5a3a74304f9323937ff63cbf15
Tim Perry <pimterry@gmail.com>
2021-04-13 14:11:01 +0200

    Release latest desktop (bug fixes & old TLS support)

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     description: 'Beautiful, cross-platform & open-source tools for debugging, testing and building with HTTP(S), on Windows, Linux & Mac.',
     siteUrl: 'https://httptoolkit.tech/',
     blogUrl: 'https://httptoolkit.tech/blog/',
-    latestAppVersion: '1.2.2'
+    latestAppVersion: '1.2.3'
   },
   plugins: [
     'gatsby-plugin-styled-components',
