ca80092a403e6a2621fa2b0760886e1e57c1a884
Tim Perry <pimterry@gmail.com>
2023-04-25 19:54:09 +0200

    Drop the old BRL block now we've resolved it payment side

--- src/accounts/account-store.js
+++ src/accounts/account-store.js
@@ -105,28 +105,6 @@ export class AccountStore {
             return;
         }
 
-        const isRiskyPayment = plan.prices?.currency === 'BRL' &&
-            this.user?.email?.endsWith('@gmail.com'); // So far, all chargebacks have been from gmail accounts
-
-        const newUser = !this.user?.subscription; // Even cancelled users will have an expired subscription left
-
-        if (newUser && isRiskyPayment) {
-            // This is annoying, I wish we didn't have to do this, but fraudulent BRL payments are now 80% of chargebacks,
-            // and we need to tighten this up and block that somehow or payment platforms will eventually block
-            // HTTP Toolkit globally. This error message is left intentionally vague to try and discourage fraudsters
-            // from using a VPN to work around it. We do still allow this for existing customers, who are already
-            // logged in - we're attempting to just block the creation of new accounts here.
-            this.reportPlanPurchaseBlocked(tierCode, planCycle);
-
-            alert(
-                "Unfortunately, due to high levels of recent chargebacks & fraud, subscriptions for new accounts "+
-                "will temporarily require manual validation & processing before setup.\n\n" +
-                "Please email purchase@httptoolkit.com to begin this process."
-            );
-
-            return;
-        }
-
         // This redirects the entire page to the checkout:
         return openCheckout(this.user.email, sku);
     }.bind(this));
