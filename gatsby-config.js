06ee3ee8a6288c42fefbd5dc141d983bfa0ea2c0
Tim Perry <pimterry@gmail.com>
2023-07-13 19:57:53 +0200

    Release latest desktop: server Send support, narrower windows, native Electron app picker

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     description: 'Beautiful, cross-platform & open-source tools for debugging, testing and building with HTTP(S), on Windows, Linux & Mac.',
     siteUrl: 'https://httptoolkit.com/',
     blogUrl: 'https://httptoolkit.com/blog/',
-    latestAppVersion: '1.12.8'
+    latestAppVersion: '1.13.0'
   },
   plugins: [
     'gatsby-plugin-styled-components',
