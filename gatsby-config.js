1395bda6dfd146ffa967ddef7add5e437141aad2
Tim Perry <pimterry@gmail.com>
2023-09-07 17:09:26 +0200

    Release v1.14.1: Chrom* & Firefox snap launch fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     description: 'Beautiful, cross-platform & open-source tools for debugging, testing and building with HTTP(S), on Windows, Linux & Mac.',
     siteUrl: 'https://httptoolkit.com/',
     blogUrl: 'https://httptoolkit.com/blog/',
-    latestAppVersion: '1.14.0'
+    latestAppVersion: '1.14.1'
   },
   plugins: [
     'gatsby-plugin-styled-components',
