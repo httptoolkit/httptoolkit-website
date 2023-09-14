21b5c62f29b504c1aa10f31811d026916d8c4ecf
Tim Perry <pimterry@gmail.com>
2023-09-14 17:39:07 +0200

    Release v1.14.2: Android 14, Deno & Node fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     description: 'Beautiful, cross-platform & open-source tools for debugging, testing and building with HTTP(S), on Windows, Linux & Mac.',
     siteUrl: 'https://httptoolkit.com/',
     blogUrl: 'https://httptoolkit.com/blog/',
-    latestAppVersion: '1.14.1'
+    latestAppVersion: '1.14.2'
   },
   plugins: [
     'gatsby-plugin-styled-components',
