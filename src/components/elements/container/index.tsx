ed89521208074444098c9560f19331a879bd128b
Gerald Martinez <gerald@bejamas.io>
2024-02-20 13:28:08 -0600

    fix: remove unnecessary forwardRef

--- src/components/elements/container/index.tsx
+++ src/components/elements/container/index.tsx
@@ -1,14 +1,5 @@
-import { forwardRef } from 'react';
-
 import { StyledContainer } from './container.styles';
 
-export const Container = forwardRef<HTMLDivElement, Component>((props, ref) => {
-  const { children, className } = props;
-  return (
-    <StyledContainer {...ref} className={className}>
-      {children}
-    </StyledContainer>
-  );
-});
-
-Container.displayName = 'Container';
+export const Container = ({ children, className }: Component) => {
+  return <StyledContainer className={className}>{children}</StyledContainer>;
+};
