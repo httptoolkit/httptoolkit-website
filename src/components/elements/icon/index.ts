423a984c13f72933a65578205478571ff3c7d606
RBoraure <58434305+RBoraurito@users.noreply.github.com>
2024-02-27 20:09:05 -0400

    [HTD-15] Feat: Add `Integration card` Component (#19)
    * feat(HTD-5): add line-height variants and text align
    
    * chore(HTD-5): add pull request template
    
    * feat(HTD-7): add base button styles
    
    * feat(HTD-7): add icons to button
    
    * feat(HTD-7): add dropdown
    
    * fix(HTD-7): update types
    
    * fix(HTD-7): change icon usage
    
    * feat(HTD-8): add badge
    
    * feat(HTD-8): add badge variants
    
    * feat(HTD-10): add square icon component
    
    * fix(HTD-7): relocate rotate keyframe
    
    * fix(HTD-7): update box-shadows
    
    * fix(HTD-7): add Aria attributes
    
    * feat(HTD-7): support NextLink and accessibility
    
    * feat(HTD-7): add target prop
    
    * fix(HTD-7): fix styles and dropdown styles and accessibility
    
    * feat(HTD-10): export  icons from component
    
    * feat(HTD-10): add logo icon
    
    * feat(HTD-11): add input
    
    * fix(HTD-7): remove unneeded reset
    
    * fix(HTD-7): update dropdown and secondary button styles
    
    * fix(HTD-7): improve border styles
    
    * fix(HTD-7): update types to make it conditional
    
    * fix(HTD-10): update shadow property
    
    * fix(HTD-10): add use client to icons
    
    * fix(HTD-10): add logo
    
    * [HTD-8] Feat: Add `Badge` component (#4)
    
    * feat(HTD-8): add badge
    
    * feat(HTD-8): add badge variants
    
    * fix(HTD-8): add missing box-shadow
    
    * fix(HTD-8): trim unused extra decimals
    
    * fix(HTD-8): add H3 tag for badge
    
    * feat(HTD-8): remove unused async
    
    * fix(HTD-10): change padding to 11px for border consistency
    
    * fix(HTD-7): add styled reset
    
    * fix(HTD-7): change as implementation
    
    * fix(HTD-10): make logo work
    
    * fix(HTD-11): improve status styles
    
    * feat(HTD-11): add text area
    
    * fix(HTD-11): improve sizing
    
    * feat(HTD-11): add use ref
    
    * fix(HTD-11): update colors
    
    * feat(HTD-11): add search
    
    * fix(HTD-11): fix badge
    
    * fix(HTD-11): update styled props
    
    * fix(HTD-11): add missing icon
    
    * fix(HTD-11): remove unneeded files
    
    * fix(HTD-11): update text area font
    
    * fix(HTD-11): add click search
    
    * feat(HTD-15): add integration card
    
    * feat(HTD-15): make badge text flexible

--- src/components/elements/icon/index.ts
+++ src/components/elements/icon/index.ts
@@ -1,5 +1,14 @@
-import { Alien, CaretDown, RocketLaunch, Sparkle, Book, MagnifyingGlass } from '@phosphor-icons/react/dist/ssr';
+import {
+  Alien,
+  CaretDown,
+  RocketLaunch,
+  Sparkle,
+  Book,
+  MagnifyingGlass,
+  AndroidLogo,
+  ArrowRight,
+} from '@phosphor-icons/react/dist/ssr';
 
 import { FullLogo, Logo } from './custom';
 
-export { Alien, CaretDown, RocketLaunch, Sparkle, Book, FullLogo, Logo, MagnifyingGlass };
+export { Alien, ArrowRight, CaretDown, RocketLaunch, Sparkle, Book, FullLogo, Logo, MagnifyingGlass, AndroidLogo };
