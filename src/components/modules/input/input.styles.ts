5267f5f36ec1b9ca1af692047bb55863cc33394b
RBoraure <58434305+RBoraurito@users.noreply.github.com>
2024-02-29 13:52:17 -0400

    [HTD-22] Feat: Add `Content card` Component (#28)
    * feat(HTD-22): add content card
    
    * fix(HTD-22): remove comment

--- src/components/modules/input/input.styles.ts
+++ src/components/modules/input/input.styles.ts
@@ -8,6 +8,7 @@ export const StyledInputWrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: 4px;
+  width: 100%;
 `;
 
 export const StyledInputBorder = styled.span<InputBorderProps>`
