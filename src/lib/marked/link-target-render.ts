42c108365d4d7cefba01e1250324238252b23280
RBoraure <58434305+RBoraurito@users.noreply.github.com>
2024-03-04 12:38:14 -0400

    [HTD-20] Fix: Change strategy for opens in a new tab label (#44)
    * fix(HTD-20): add label for target blank links
    
    * fix(HTD-20): change strategy for opens in a new tab

--- src/lib/marked/link-target-render.ts
+++ src/lib/marked/link-target-render.ts
@@ -4,5 +4,7 @@ export const renderer = new marked.Renderer();
 const linkRenderer = renderer.link;
 renderer.link = (href, title, text) => {
   const html = linkRenderer.call(renderer, href, title, text);
-  return html.replace(/^<a /, `<a target="_blank" aria-label="opens in a new tab" rel="noopener noreferrer" `);
+  return html
+    .replace(/^<a /, `<a target="_blank" rel="noopener noreferrer" `)
+    .replace(/<\/a>/, `<span class="visually-hidden">opens in a new tab</span></a>`);
 };
