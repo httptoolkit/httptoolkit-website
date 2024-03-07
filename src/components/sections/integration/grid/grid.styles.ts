a836499454daff4d41792b458d53af698ce80258
RBoraure <58434305+RBoraurito@users.noreply.github.com>
2024-03-07 09:09:16 -0400

    [HTD-38] Feat: Add `Integration grid` component (#53)
    * feat(HTD-38): add integration grid
    
    * fix(HTD-38): change props for grid
    
    * fix(HTD-38): change props name
    
    * feat(HTD-38): add key prop

---
+++ src/components/sections/integration/grid/grid.styles.ts
@@ -0,0 +1,14 @@
+'use client';
+
+import { styled } from '@/styles';
+
+export const StyledIntegrationGrid = styled.section`
+  display: grid;
+  gap: 16px;
+
+  @media (min-width: ${({ theme }) => theme.screens.lg}) {
+    padding: 48px 0;
+    gap: 20px;
+    grid-template-columns: repeat(3, 1fr);
+  }
+`;
