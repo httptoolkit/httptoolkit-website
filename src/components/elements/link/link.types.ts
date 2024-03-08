a8eacbaaa383f26bff9624040a5d7241c3d3ed7a
RBoraure <58434305+RBoraurito@users.noreply.github.com>
2024-03-08 09:05:52 -0400

    fix(HTD-15): add accessible name to integration card link (#64)

--- src/components/elements/link/link.types.ts
+++ src/components/elements/link/link.types.ts
@@ -1,9 +1,9 @@
 import type { LinkProps as NextLinkProps } from 'next/link';
-import type { AnchorHTMLAttributes } from 'react';
+import type { AnchorHTMLAttributes, AriaAttributes } from 'react';
 
 export type NextLinkType = Omit<NextLinkProps, 'href'>;
 
-export type AnchorProps = AnchorHTMLAttributes<HTMLAnchorElement>;
+export type AnchorProps = AnchorHTMLAttributes<HTMLAnchorElement> & AriaAttributes;
 
 export type LinkProps = {
   href: string;
