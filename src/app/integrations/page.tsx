18ab61ec6faa84c46be037b695cb736adcad18c0
RBoraure <58434305+RBoraurito@users.noreply.github.com>
2024-03-11 17:48:48 -0400

    feat(HTD-40): add device media section (#74)

--- src/app/integrations/page.tsx
+++ src/app/integrations/page.tsx
@@ -1,6 +1,7 @@
 import { Container } from '@/components/elements/container';
 import { Heading } from '@/components/elements/heading';
 import { IntegrationCompatibility } from '@/components/sections/integration/single-page/compatibility';
+import { IntegrationDeviceMedia } from '@/components/sections/integration/single-page/device-media';
 
 export default function IntegrationsPage() {
   return (
@@ -8,6 +9,18 @@ export default function IntegrationsPage() {
       <Container>
         <Heading color="textGradient">Integrations</Heading>
       </Container>
+      <IntegrationDeviceMedia
+        mobileImage={{
+          darkSrc: 'https://picsum.photos/id/1019/1024',
+          lightSrc: 'https://picsum.photos/id/1035/1024',
+          alt: 'example alt',
+        }}
+        desktopImage={{
+          darkSrc: 'https://picsum.photos/id/1019/1024',
+          lightSrc: 'https://picsum.photos/id/1035/1024',
+          alt: 'example alt',
+        }}
+      />
       <IntegrationCompatibility title="Supported by Docker" subtitle="compatibility" />
     </>
   );
