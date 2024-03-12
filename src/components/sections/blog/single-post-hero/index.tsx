de54583243b7e48ee9c90650805b8404ea0fb72f
Gerald Martinez <gerald@bejamas.io>
2024-03-12 10:34:01 -0600

    fix(HTD-53): go back color text

--- src/components/sections/blog/single-post-hero/index.tsx
+++ src/components/sections/blog/single-post-hero/index.tsx
@@ -25,7 +25,7 @@ const GoBack = ({ $displayOn }: { $displayOn: 'desktop' | 'mobile' }) => {
     <StyledGoBack $displayOn={$displayOn}>
       <Link href="/blog">
         <CaretLeft weight="fill" />
-        <Text as="label" fontSize="m" fontWeight="bold">
+        <Text as="label" fontSize="m" fontWeight="bold" color="white">
           GO BACK TO BLOG
         </Text>
       </Link>
