dde7620ac0029291736dd4bbfccdb8d6a9f522fd
RBoraure <58434305+RBoraurito@users.noreply.github.com>
2024-03-12 09:28:29 -0400

    [HTD-45] Feat: Add `Feature Bento Grid` Component (#73)
    * feat(HTD-25): add feature bento grid
    
    * fix(HTD-45): chunk array

--- src/components/modules/fluid-card/index.tsx
+++ src/components/modules/fluid-card/index.tsx
@@ -7,17 +7,19 @@ import { Button } from '@/components/elements/button';
 import { Heading } from '@/components/elements/heading';
 import type { HeadingProps } from '@/components/elements/heading/heading.types';
 import { SquareIcon } from '@/components/elements/square-icon';
+import type { SquareIconProps } from '@/components/elements/square-icon/square-icon.types';
 import { renderer } from '@/lib/marked/link-target-render';
 
 export const FluidCard = ({ title, icon, text, buttonHref, buttonText, $variant = 'default' }: FluidCardProps) => {
   const headingSize: HeadingProps['fontSize'] = $variant === 'dark' ? 's' : 'm';
   const headingColor: HeadingProps['color'] = $variant === 'highlighted' ? 'alwayWhite' : 'lightGrey';
   const headingWeight: HeadingProps['fontWeight'] = $variant === 'dark' ? 'medium' : 'normal';
+  const iconSize: SquareIconProps['$size'] = $variant === 'dark' ? 'medium' : 'large';
 
   return (
     <StyledFluidCardWrapper $variant={$variant}>
       {icon && (
-        <SquareIcon icon={icon} $variant={$variant !== 'default' ? 'tertiary' : 'tertiary-bigger'} $size="medium" />
+        <SquareIcon icon={icon} $variant={$variant !== 'default' ? 'tertiary' : 'tertiary-bigger'} $size={iconSize} />
       )}
       <StyledFluidCardContentWrapper $variant={$variant}>
         <Heading as="h3" color={headingColor} fontSize={headingSize} fontWeight={headingWeight}>
