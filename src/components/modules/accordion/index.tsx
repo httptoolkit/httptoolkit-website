21be3ce40fb00d1e4b652eb6550131609cfae1e6
RBoraure <58434305+RBoraurito@users.noreply.github.com>
2024-03-14 12:58:43 -0400

    [HTD-59] Fix: FAQ qa fixes (#87)
    * fix(HTD-59): change button size
    
    * fix(HTD-59): change heading tag
    
    * fix(HTD-59): remove double header
    
    * fix(HTD-59): change text size and colors

--- src/components/modules/accordion/index.tsx
+++ src/components/modules/accordion/index.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import * as RadixAccordion from '@radix-ui/react-accordion';
-
 import {
   StyledAccordionContent,
   StyledAccordionItem,
@@ -12,6 +10,7 @@ import type { AccordionProps } from './accordion.types';
 
 import { Heading } from '@/components/elements/heading';
 import { CaretDown } from '@/components/elements/icon';
+import { Text } from '@/components/elements/text';
 
 export const Accordion = ({ items, $variant = 'default' }: AccordionProps) => {
   return (
@@ -20,15 +19,17 @@ export const Accordion = ({ items, $variant = 'default' }: AccordionProps) => {
         items.length > 0 &&
         items.map(item => (
           <StyledAccordionItem value={item.title} $variant={$variant}>
-            <RadixAccordion.Header>
-              <StyledAccordionTrigger>
-                <Heading as="h3" fontSize="s" color="darkGrey" textAlign="left">
-                  {item.title}
-                </Heading>
-                <CaretDown weight="fill" size={24} />
-              </StyledAccordionTrigger>
-            </RadixAccordion.Header>
-            <StyledAccordionContent>{item.text}</StyledAccordionContent>
+            <StyledAccordionTrigger>
+              <Heading as="h3" fontSize="m" color="darkGrey" textAlign="left">
+                {item.title}
+              </Heading>
+              <CaretDown weight="fill" size={24} />
+            </StyledAccordionTrigger>
+            <StyledAccordionContent>
+              <Text fontSize="m" color="darkGrey">
+                {item.text}
+              </Text>
+            </StyledAccordionContent>
           </StyledAccordionItem>
         ))}
     </StyledAccordionWrapper>
