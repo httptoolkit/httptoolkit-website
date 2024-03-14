dfb8326921cca2411078b47a0a506a0a94cee953
RBoraure <58434305+RBoraurito@users.noreply.github.com>
2024-03-14 09:12:53 -0400

    [HTD-41] Feat: add integration bento (#67)
    * feat(HTD-41): add integration bento
    Waiting for phone implementation and gradient
    
    * fix(HTD-41): add fallback to container
    
    * fix(HTD-41): move components to integration page
    
    * fix(HTD-41): change image
    
    * feat(HTD-41): add right gradient
    
    * fix(HTD-41): add new optimized image

---
+++ src/components/sections/integration/single-page/bento/bento.types.ts
@@ -0,0 +1,8 @@
+import type { FluidCardProps } from '@/components/modules/fluid-card/fluid-card.types';
+
+export interface IntegrationBentoProps {
+  title: string;
+  subtitle: string;
+  phone?: any;
+  cards: FluidCardProps[];
+}
