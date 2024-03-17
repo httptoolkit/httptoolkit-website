8042c62d03101a8047b29950c14e4637c6a2b980
Gerald Martinez <gerald@bejamas.io>
2024-03-17 14:42:23 -0600

    feat(HTD-71): build product pages with common sections (#94)
    * feat(HTD-71): build product pages with common sections
    
    * feat(HTD-71): add ProductLdData metadata
    
    * feat(HTD-71): add basic metadata title, description

--- src/app/(products)/http-toolkit-for-macos/page.tsx
+++ src/app/(products)/http-toolkit-for-macos/page.tsx
@@ -1,5 +1,20 @@
+import type { Metadata } from 'next/types';
+import { Suspense } from 'react';
+
+import { HttpToolkitFeatures } from '@/components/common-sections/http-toolkit-features';
+import { MockResponseFeatures } from '@/components/common-sections/mock-response-features';
+import { RewriteAnything } from '@/components/common-sections/rewrite-anything';
+import { TryItForYourselfCTA } from '@/components/common-sections/try-it-for-yourself';
 import { AppleLogo } from '@/components/elements/icon';
+import { ProductLdData } from '@/components/elements/product-ld-data';
 import { CTA } from '@/components/sections/cta';
+import { Testimonials } from '@/components/sections/testimonials';
+
+export const metadata: Metadata = {
+  title: 'Intercept, debug & mock HTTP(S) on macOS',
+  description:
+    'HTTP Toolkit lets you easily see and modify HTTP & HTTPS messages from applications, browsers, scripts & more on your macOS computer.',
+};
 
 export default async function ForMacOsPage() {
   return (
@@ -13,10 +28,19 @@ export default async function ForMacOsPage() {
           darkSrc: '/images/hero-placeholder-dark.webp',
           lightSrc: '/images/hero-placeholder-light.webp',
           withBorderAnimation: true,
-          alt: 'Hero',
           loading: 'eager',
         }}
       />
+      <HttpToolkitFeatures />
+      <MockResponseFeatures />
+      <RewriteAnything />
+      <Suspense>
+        <Testimonials />
+      </Suspense>
+      <TryItForYourselfCTA variant="cta-fluid" />
+      <Suspense>
+        <ProductLdData />
+      </Suspense>
     </>
   );
 }
