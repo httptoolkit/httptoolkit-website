7e0ee710c7db5eb2176222cdbc7efae169010343
RBoraure <58434305+RBoraurito@users.noreply.github.com>
2024-03-21 12:15:14 -0400

    fix(HTD-60): remove font weight increase on hover focus or active (#114)

--- src/components/modules/table-content/table-content.styles.ts
+++ src/components/modules/table-content/table-content.styles.ts
@@ -99,12 +99,10 @@ export const StyledTableContentSubitem = styled(Link)`
 
   &:hover,
   &:focus {
-    font-weight: ${({ theme }) => theme.fontWeight.medium};
     color: ${({ theme }) => theme.colors.white};
   }
 
   &:active {
-    font-weight: ${({ theme }) => theme.fontWeight.bold};
     color: ${({ theme }) => theme.colors.electricLightBlue};
   }
 `;
