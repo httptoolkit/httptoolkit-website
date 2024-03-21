8227996b579a08d7dfe41f18d480cb213a3744e6
Gerald Martinez <gerald@bejamas.io>
2024-03-21 08:59:54 -0600

    fix(HTD-87): bottom spacing

--- src/components/elements/section/index.tsx
+++ src/components/elements/section/index.tsx
@@ -1,4 +1,4 @@
-import { StyledSection } from './container.styles';
+import { StyledSection } from './section.styles';
 
 export const Section = ({ children, className }: Component) => {
   return <StyledSection className={className}>{children}</StyledSection>;
