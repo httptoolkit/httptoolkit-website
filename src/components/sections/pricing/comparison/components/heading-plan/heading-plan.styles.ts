c649316b80275f86a348ac59dd4d6cd0df26eb0e
RBoraure <58434305+RBoraurito@users.noreply.github.com>
2024-04-03 08:54:02 -0400

    fix(HTD-91): change download button padding on comparison (#128)
    * fix(HTD-91): change download button padding on comparison
    
    * fix(HTD-91): make selector more comprensible

--- src/components/sections/pricing/comparison/components/heading-plan/heading-plan.styles.ts
+++ src/components/sections/pricing/comparison/components/heading-plan/heading-plan.styles.ts
@@ -15,8 +15,8 @@ export const StyledHeadingPlanWrapper = styled.div`
       width: 100%;
     }
 
-    & button,
-    & a {
+    & button[data-dropdown='true'],
+    & a[data-dropdown='true'] {
       width: 100%;
       padding-left: 0;
       padding-right: 0;
