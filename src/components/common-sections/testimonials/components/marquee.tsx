8a9a7ffc7297680a2b9614a35608428dce4b5c4e
Gerald Martinez <gerald@bejamas.io>
2024-04-14 10:35:40 -0600

    chore: update next to latest version (#145)
    * chore: update next to latest version
    
    * fix: build
    
    * fix: add marque with next dynamic
    
    * chore: bump nextjs

--- src/components/common-sections/testimonials/components/marquee.tsx
+++ src/components/common-sections/testimonials/components/marquee.tsx
@@ -1,9 +1,13 @@
 'use client';
 
+import dynamic from 'next/dynamic';
 import type { ReactNode } from 'react';
-import Marquee from 'react-fast-marquee';
 
 import { useIsMobile } from '@/lib/hooks/use-is-mobile';
+
+const Marquee = dynamic(() => import('react-fast-marquee').then(e => e.default), {
+  ssr: false,
+});
 
 interface MarqueeWrapperProps {
   children: ReactNode;
