7a38b65844fcba62a300f1bff1120fc603acbcc2
Tim Perry <pimterry@gmail.com>
2024-05-02 17:56:19 +0200

    Release v1.15.0: trailers, HTTP/2 & ADB fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     description: 'Beautiful, cross-platform & open-source tools for debugging, testing and building with HTTP(S), on Windows, Linux & Mac.',
     siteUrl: 'https://httptoolkit.com/',
     blogUrl: 'https://httptoolkit.com/blog/',
-    latestAppVersion: '1.14.11',
+    latestAppVersion: '1.15.0',
     latestSiteUpdate: new Date().toISOString() // I.e. the date of the latest site build
   },
   plugins: [
