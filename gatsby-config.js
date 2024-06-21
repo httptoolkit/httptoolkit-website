6805d7cf39444c94d9805887fe34c9ec45f0f264
Tim Perry <pimterry@gmail.com>
2024-06-21 18:35:07 +0200

    Release v1.17.2: assorted small fixes, including ADB script issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     description: 'Beautiful, cross-platform & open-source tools for debugging, testing and building with HTTP(S), on Windows, Linux & Mac.',
     siteUrl: 'https://httptoolkit.com/',
     blogUrl: 'https://httptoolkit.com/blog/',
-    latestAppVersion: '1.17.1',
+    latestAppVersion: '1.17.2',
     latestSiteUpdate: new Date().toISOString() // I.e. the date of the latest site build
   },
   plugins: [
