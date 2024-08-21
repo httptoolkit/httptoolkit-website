c3b3527892e4ac94a789b6254305b976ca67d07a
Tim Perry <pimterry@gmail.com>
2024-08-21 17:01:43 +0200

    Stop theme-irrelevant videos from loading

--- src/components/elements/video-player/index.tsx
+++ src/components/elements/video-player/index.tsx
@@ -1,3 +1,10 @@
+'use client';
+
+import * as React from "react";
+import { useTheme } from "next-themes";
+
+import { useMounted } from '@/lib/hooks/use-mounted';
+
 import { StyledIframe } from "./video-player.styles";
 
 const urlOptions = new URLSearchParams({
@@ -14,23 +21,47 @@ export const VideoPlayer = (props: {
     lightId: string,
     aspectRatio: string
 }) => {
+    const { isMounted } = useMounted();
+    const { resolvedTheme: theme } = useTheme();
+
     const darkUrl = `https://iframe.mediadelivery.net/embed/${props.libraryId}/${props.darkId}?${urlOptions}`;
     const lightUrl = `https://iframe.mediadelivery.net/embed/${props.libraryId}/${props.lightId}?${urlOptions}`;
 
+    const darkVisibility = isMounted
+        ? theme === 'dark'
+        : undefined;
+    const lightVisibility = isMounted
+        ? theme === 'light'
+        : undefined;
+
     return <>
-        <StyledIframe
-            src={lightUrl}
-            data-hide-on-theme="dark"
-            aspectRatio={props.aspectRatio}
-            allow="autoplay;picture-in-picture;"
-            allowFullScreen={true}
-        />
-        <StyledIframe
-            src={darkUrl}
-            data-hide-on-theme="light"
-            aspectRatio={props.aspectRatio}
-            allow="autoplay;picture-in-picture;"
-            allowFullScreen={true}
-        />
+        { (!isMounted || theme === 'dark') &&
+            <StyledIframe
+                src={darkUrl}
+
+                loading="lazy"
+                // Hide if wrong theme
+                data-hide-on-theme="light"
+                $visible={darkVisibility}
+
+                $aspectRatio={props.aspectRatio}
+                allow="autoplay;picture-in-picture;"
+                allowFullScreen={true}
+            />
+        }
+        { (!isMounted || theme === 'light') &&
+            <StyledIframe
+                src={lightUrl}
+
+                loading="lazy"
+                // Hide if wrong theme
+                data-hide-on-theme="dark"
+                $visible={lightVisibility}
+
+                $aspectRatio={props.aspectRatio}
+                allow="autoplay;picture-in-picture;"
+                allowFullScreen={true}
+            />
+        }
     </>;
 };
