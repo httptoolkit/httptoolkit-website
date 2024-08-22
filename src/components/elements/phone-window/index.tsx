646b42231600a530cbdf665837a60b66835f6f56
Tim Perry <pimterry@gmail.com>
2024-08-22 19:02:49 +0200

    Add a phone window video container

---
+++ src/components/elements/phone-window/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import {
+    PhoneOutline,
+    PhoneScreen,
+    HomeButton
+} from './phone-window.styles';
+
+export const PhoneWindow = (p: {
+    aspectRatio: string;
+    children: React.ReactNode;
+}) =>
+    <PhoneOutline>
+        <PhoneScreen $aspectRatio={p.aspectRatio}>
+            { p.children }
+        </PhoneScreen>
+        <HomeButton />
+    </PhoneOutline>;
