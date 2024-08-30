314a16026a609118acd5a27e38ca745908a8c691
Tim Perry <pimterry@gmail.com>
2024-08-30 19:01:00 +0200

    Release v1.19.0: rebrand & global Chromium

--- src/lib/site-metadata.ts
+++ src/lib/site-metadata.ts
@@ -6,7 +6,7 @@ export const siteMetadata = {
   siteUrl: 'https://httptoolkit.com',
   blogUrl: 'https://httptoolkit.com/blog',
 
-  latestAppVersion: '1.18.1',
+  latestAppVersion: '1.19.0',
   latestSiteUpdate: new Date().toISOString(), // I.e. the date of the latest site build
 
   // Fallback static stats in case we hit the github api limits (only used in dev)
