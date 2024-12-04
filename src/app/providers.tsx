2d30ebc28f6c6111a27ced06f47d8bc1cfdcd74c
Tim Perry <pimterry@gmail.com>
2024-12-04 14:13:40 +0100

    Skip PH setup if the key isn't set, for local testing

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,7 +3,7 @@
 import posthog from 'posthog-js';
 import { PostHogProvider } from 'posthog-js/react';
 
-if (typeof window !== 'undefined') {
+if (typeof window !== 'undefined' && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host: 'https://events.httptoolkit.tech',
     autocapture: false, // We don't need events here - just page views is fine.
