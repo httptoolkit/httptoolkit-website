0ae0a04ee2f32b476267cc26e7cad90ec86f0f04
Tim Perry <pimterry@gmail.com>
2026-03-10 15:24:03 +0100

    Simplify the replaceAll polyfill setup

--- src/components/elements/polyfills/index.tsx
+++ src/components/elements/polyfills/index.tsx
@@ -2,7 +2,21 @@
 
 // Polyfill for older browsers (more common here than we'd expect, due to testing
 // on old Android emulators etc):
-import 'core-js/actual/string/replace-all';
+if (!String.prototype.replaceAll) {
+    String.prototype.replaceAll = function (
+        this: string,
+        search: string | RegExp,
+        replacement: string | ((substring: string, ...args: unknown[]) => string),
+    ) {
+        if (search instanceof RegExp) {
+            if (!search.global) {
+                throw new TypeError('String.prototype.replaceAll called with a non-global RegExp argument');
+            }
+            return this.replace(search, replacement as string);
+        }
+        return this.split(search).join(replacement as string);
+    } as typeof String.prototype.replaceAll;
+}
 
 export function Polyfills() {
     return null;
